a6936242f651a1fab123f300cc18a83c7d98491b
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-06-22 09:16:00 -0300

    Criação do footer e renderização dele no App

--- 02-Front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/content/src/App.js
+++ 02-Front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/content/src/App.js
@@ -1,11 +1,13 @@
 import Header from './header';
 import Content from './content';
+import Footer from './footer';
 
 function App() {
   return (
     <>
       <Header />
       <Content />
+      <Footer />
     </>
   );
 }
