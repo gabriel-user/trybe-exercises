40b18d021c822211eb14a5d4b73149eb62df9ad4
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-06-21 22:26:31 -0300

    Criação da lista de compromisso

--- 02-Front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/to-do-list/src/App.js
+++ 02-Front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/to-do-list/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import './App.css';
 
+const compromissos = [
+  'Exercitar-se por 30 minutos',
+  'Afirmações e visualizações',
+  'Ler 10 páginas de um livro',
+  'Arrumar a casa',
+  'Monitoria Summer'
+];
+
+
 const Task = (value) => {
   return (
     <li>{value}</li>
