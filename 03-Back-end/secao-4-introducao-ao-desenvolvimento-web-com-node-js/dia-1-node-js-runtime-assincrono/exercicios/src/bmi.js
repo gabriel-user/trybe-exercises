e23fb64a000f6adb85d97402c72e0df94ef19766
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-11-01 11:41:55 -0300

    Adiciona função de categoria de IMC - Exercício 5

--- 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/exercicios/src/bmi.js
+++ 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-runtime-assincrono/exercicios/src/bmi.js
@@ -6,15 +6,25 @@ const handleBmi = (weight, height) => {
   const bmi = Number(weight) / heightElevated;
 
   return bmi;
-}
+};
+
+const bmiCategory = (bmi) => {
+  if (bmi < 18.5) return 'Abaixo do peso (magreza)';
+  if (bmi >= 18.5 && bmi <= 24.9) return 'Peso normal';
+  if (bmi >= 25.0 && bmi <= 29.9) return 'Acima do peso (sobrepeso)';
+  if (bmi >= 30.0 && bmi <= 34.9) return 'Obesidade grau I';
+  if (bmi >= 35.0 && bmi <= 39.9) return 'Obesidade grau II';
+  if (bmi >= 40.0) return 'Obesidade graus III e IV';
+};
 
 const main = () => {
   const weight = readline.questionFloat('What your weight? ');
   const height = readline.questionFloat('What your height? ');
   const bmi = handleBmi(weight, height);
+  const category = bmiCategory(bmi);
 
-  console.log(`Your BMI is ${ bmi }`);
-}
+  console.log(`Your BMI is ${bmi} - ${ category }`);
+};
 
 main();
 
