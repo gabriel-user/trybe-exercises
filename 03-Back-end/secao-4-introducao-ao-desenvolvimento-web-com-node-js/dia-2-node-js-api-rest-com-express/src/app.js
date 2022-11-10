f5070ed70f6f8d2571f29010987ca3e872cd26fb
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-11-10 19:42:31 -0300

    Refatoraçao do arquivo app.js

--- 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-api-rest-com-express/src/app.js
+++ 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-api-rest-com-express/src/app.js
@@ -8,54 +8,60 @@ app.use(express.json());
 const MOVIES_FILE_PATH = resolve(__dirname, './movies.json');
 
 app.put('/movies/:id', async (req, res) => {
-  const id = Number(req.params.id);
-  const movies = await readFileJson(MOVIES_FILE_PATH);
-  const movieToUpdate = movies.find((movie) => Number(movie.id) === id);
-  const indexOfMovieToUpdate = movies.indexOf(movieToUpdate);
-  const newMovieInfo = { id, ...req.body };
-  movies.splice(indexOfMovieToUpdate, 1, newMovieInfo);
-  await writeFileJson(MOVIES_FILE_PATH, movies);
-
-  res.status(200).json(newMovieInfo);
+  try {
+    const id = Number(req.params.id);
+    const movies = await (readFileJson(MOVIES_FILE_PATH));
+    const index = movies.findIndex((movie) => Number(movie.id) === id);
+    movies[index] = { id, ...req.body };
+    await writeFileJson(MOVIES_FILE_PATH, movies);
+    res.status(200).json(movies[index]);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 app.get('/movies/:id', async (req, res) => {
-  const { id } = req.params;
-  const movies = await readFileJson(MOVIES_FILE_PATH);
-  const movieResult = movies.find(
-    (movie) => Number(movie.id) === Number(id),
-  );
-
-  if (!movieResult) {
-    return res.status(400).json({
-      message: 'Não foi possível encontrar um filme com o ID informado.',
-    });
+  try {
+    const { id } = req.params;
+    const movies = await readFileJson(MOVIES_FILE_PATH);
+    const movieResult = movies.find((movie) => Number(movie.id) === Number(id));
+    res.status(200).json({ movieResult });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
   }
-
-  res.status(200).json({ movieResult });
 });
 
 app.get('/movies', async (_req, res) => {
-  const movies = await readFileJson(MOVIES_FILE_PATH);
-  res.status(200).json({ movies });
+  try {
+    const movies = await readFileJson(MOVIES_FILE_PATH);
+    res.status(200).json({ movies });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 app.post('/movies', async (req, res) => {
-  const movies = await readFileJson(MOVIES_FILE_PATH);
-  const newMovie = { id: movies.length + 1, ...req.body }; 
-  movies.push(newMovie);
-  await writeFileJson(MOVIES_FILE_PATH, movies);
-
-  res.status(200).json(newMovie);
+  try {
+    const movies = await readFileJson(MOVIES_FILE_PATH);
+    const newMovie = { id: movies.length + 1, ...req.body };
+    movies.push(newMovie);
+    await writeFileJson(MOVIES_FILE_PATH, movies);
+    res.status(201).json(newMovie);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 app.delete('/movies/:id', async (req, res) => {
-  const { id } = req.params;
-  const movies = await readFileJson(MOVIES_FILE_PATH);
-  const newMoviesArray = movies.filter((movie) => Number(movie.id) !== Number(id));
-  await writeFileJson(MOVIES_FILE_PATH, newMoviesArray);
-
-  res.status(200).end();
+  try {
+    const { id } = req.params;
+    const movies = await readFileJson(MOVIES_FILE_PATH);
+    const newMoviesArray = movies.filter((movie) => Number(movie.id) !== Number(id));
+    await writeFileJson(MOVIES_FILE_PATH, newMoviesArray);
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 module.exports = app;
