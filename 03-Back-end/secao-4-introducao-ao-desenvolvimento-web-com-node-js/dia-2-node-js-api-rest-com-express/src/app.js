570c862537ba040998e9a9194e330f631be200e8
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-11-10 16:36:23 -0300

    Exercicio 5

--- 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-api-rest-com-express/src/app.js
+++ 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-2-node-js-api-rest-com-express/src/app.js
@@ -1,5 +1,26 @@
 const express = require('express');
+const { resolve } = require('path');
+const { readFileJson } = require('./utils/fsUtils');
 
 const app = express();
+app.use(express.json());
+
+const MOVIES_FILE_PATH = resolve(__dirname, './movies.json');
+
+app.get('/movies/:id', async (req, res) => {
+  const { id } = req.params;
+  const movies = await readFileJson(MOVIES_FILE_PATH);
+  const movieResult = movies.find(
+    (movie) => Number(movie.id) === Number(id),
+  );
+
+  if (!movieResult) {
+    return res.status(400).json({
+      message: 'Não foi possível encontrar um filme com o ID informado.',
+    });
+  }
+
+  res.status(200).json({ movieResult });
+});
 
 module.exports = app;
