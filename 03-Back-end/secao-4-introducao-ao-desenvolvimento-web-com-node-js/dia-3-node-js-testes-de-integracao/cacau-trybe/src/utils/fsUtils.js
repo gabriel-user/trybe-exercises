898d091fd7716111f3b984582c965a5c5b60d80a
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-11-11 18:56:24 -0300

    Criaçao do arquivo fsUtils e finalizacao do primeiro endPoint - cacau-trybe

---
+++ 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-3-node-js-testes-de-integracao/cacau-trybe/src/utils/fsUtils.js
@@ -0,0 +1,13 @@
+const { readFile } = require('fs').promises;
+
+const readFileJson = async (filePath) => {
+  try {
+    const fileData = await readFile(filePath, 'utf-8');
+    return JSON.parse(fileData);
+  } catch (error) {
+    console.error(`Erro na leitura do arquivo: ${error}`);
+    return null;
+  }
+};
+
+module.exports = { readFileJson };
