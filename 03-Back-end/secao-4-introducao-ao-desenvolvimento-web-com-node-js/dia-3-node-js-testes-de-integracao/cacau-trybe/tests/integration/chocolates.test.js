0557f79f63654fce92d8ea0e50a038273e04b2a4
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-11-12 09:47:40 -0300

    Criacao d teste para /chocolates/:id usando TDD - cacau-trybe

--- 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-3-node-js-testes-de-integracao/cacau-trybe/tests/integration/chocolates.test.js
+++ 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-3-node-js-testes-de-integracao/cacau-trybe/tests/integration/chocolates.test.js
@@ -9,7 +9,7 @@ const { expect } = chai;
 
 chai.use(chaiHttp);
 
-const mockFile = JSON.stringify({ 
+const mockFile = JSON.stringify({
   brands: [
     {
       id: 1,
@@ -53,25 +53,36 @@ describe('Testaando a API Cacau Trybe', function () {
     sinon.stub(fs.promises, 'readFile').resolves(mockFile);
   });
 
-  afterEach(function() {
+  afterEach(function () {
     sinon.restore();
   });
 
   describe('Usando o método GET em /chocolates', function () {
-    it ('Retorna a lista completa de chocolates', async function () {
+    it('Retorna a lista completa de chocolates', async function () {
       const output = [
-        { "id": 1, "name": "Mint Intense", "brandId": 1 },
-        { "id": 2, "name": "White Coconut", "brandId": 1 },
-        { "id": 3, "name": "Mon Chéri", "brandId": 2 },
-        { "id": 4, "name": "Mounds", "brandId": 3 }
+        { id: 1, name: 'Mint Intense', brandId: 1 },
+        { id: 2, name: 'White Coconut', brandId: 1 },
+        { id: 3, name: 'Mon Chéri', brandId: 2 },
+        { id: 4, name: 'Mounds', brandId: 3 },
       ];
-      
-      const response = await chai
-        .request(app)
-        .get('/chocolates');
-      
+
+      const response = await chai.request(app).get('/chocolates');
+
       expect(response.status).to.be.equal(200);
       expect(response.body.chocolates).to.deep.equal(output);
+    });
+  });
+
+  describe('Usando o método GET em /chocolates/:id para buscar o ID 4', function () {
+    it('Retorna o chocolate Mounds', async function () {
+      const response = await chai.request(app).get('/chocolates/4');
+
+      expect(response.status).to.be.equal(200);
+      expect(respose.body.chocolate).to.deep.equal({
+        id: 4,
+        name: 'Mounds',
+        brandId: 3,
+      });
     });
   });
 });
