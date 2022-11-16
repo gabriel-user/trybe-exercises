bdd3598ed34f94814ed9a7b7dba0d260276976da
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-11-16 10:24:48 -0300

    Adicao de validacao de campos no endPont post na rota /signup - Parte 1 - Exercicio 2

--- 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/src/routers/signUpRouter.js
+++ 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/src/routers/signUpRouter.js
@@ -2,7 +2,13 @@ const router = require('express').Router();
 
 const tokenGenerator = require('../utils/tokenGenerator');
 
-router.post('/', (_req, res) => {
+router.post('/', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
+  
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(401).json({ message: 'Campos ausentes!' });
+  }
+
   const token = tokenGenerator(8);
   res.status(200).json({ token });
 });
