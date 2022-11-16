9be4ac053a1ad801157ff03d345a9f95e2cc31dc
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-11-16 09:42:43 -0300

    Criacao do middleware validateCreatedAt - Parte 7 - Exercicio 1

--- 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/src/routers/activitiesRouter.js
+++ 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/src/routers/activitiesRouter.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
 
+const validateCreatedAt = require('../middlewares/validateCreatedAt');
+const validateDescription = require('../middlewares/validateDescription');
 const validateName = require('../middlewares/validateName');
 const validatePrice = require('../middlewares/validatePrice');
 
-router.post('/', validateName, validatePrice, (_req, res) => {
+router.post('/', 
+  validateName, 
+  validatePrice, 
+  validateDescription,
+  validateCreatedAt,
+  (_req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
 
