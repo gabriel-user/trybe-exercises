48657b91955e061f81c8ec91b2ca2b7ece5ac1c0
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-11-16 09:48:07 -0300

    Criacao do middleware validateRating - Parte 8 - Exercicio 1

--- 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/src/routers/activitiesRouter.js
+++ 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/src/routers/activitiesRouter.js
@@ -4,12 +4,14 @@ const validateCreatedAt = require('../middlewares/validateCreatedAt');
 const validateDescription = require('../middlewares/validateDescription');
 const validateName = require('../middlewares/validateName');
 const validatePrice = require('../middlewares/validatePrice');
+const validateRating = require('../middlewares/validateRating');
 
 router.post('/', 
   validateName, 
   validatePrice, 
   validateDescription,
   validateCreatedAt,
+  validateRating,
   (_req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
