d7eedb39164746d828f859c716641d4db462b91d
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-11-16 09:23:36 -0300

    Criacao do middleware validateDescription - Parte 5 - Exercicio 1

--- 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/src/routers/activitiesRouter.js
+++ 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-4-node-js-express-e-middlewares/src/routers/activitiesRouter.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
 const validateName = require('../middlewares/validateName');
+const validatePrice = require('../middlewares/validatePrice');
 
-router.post('/', validateName, (_req, res) => {
+router.post('/', validateName, validatePrice, (_req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
 
