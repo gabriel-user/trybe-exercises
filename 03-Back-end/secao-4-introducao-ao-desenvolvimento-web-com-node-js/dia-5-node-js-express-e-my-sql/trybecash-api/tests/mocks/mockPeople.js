10560905b0995cc90224a93c1ef20d9b280ccb83
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-11-18 17:39:56 -0300

    Refatoracao no arquivo people.test.js

---
+++ 03-Back-end/secao-4-introducao-ao-desenvolvimento-web-com-node-js/dia-5-node-js-express-e-my-sql/trybecash-api/tests/mocks/mockPeople.js
@@ -0,0 +1,18 @@
+const mockPeople = [
+  {
+    id: 1,
+    firstName: 'Luke',
+    lastName: 'Skywalker',
+    email: 'luke.skywalker@trybe.com',
+    phone: '851 678 4453',
+  },
+  {
+    id: 2,
+    firstName: 'Dart',
+    lastName: 'Vader',
+    email: 'dart.vader@trybe.com',
+    phone: '851 678 5665',
+  },
+];
+
+module.exports = mockPeople;
