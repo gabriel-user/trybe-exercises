0fe497bfbda872f70b151e884b08bc604d7e0ac9
Gabriel Vinicius <gabrielvinicius.ecom@gmail.com>
2022-12-06 16:56:15 -0300

    Adiçao do Readme do dia 6.2

---
+++ 03-Back-end/secao-6-node-js-orm-e-autenticacao/dia-2-orm-associations-1-:-1/app-conteudo-do-course/src/migrations/20221204151352-create-addresses.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('addresses', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      employeeId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        // Configuram o que deve acontecer ao atualizar ou excluir um usuário
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'employee_id',
+        // Informa que o campo é uma Foreign Key (Chave estrangeira)
+        references: {
+          // Informa a tabela da referência da associação
+          model: 'employees',
+          // Informa a coluna da referência que é a chave correspondente
+          key: 'id',
+        }
+      },
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('addresses');
+  }
+};
